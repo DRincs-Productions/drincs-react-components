8f1377fee04f6e0df96a5c854255760ff7193655
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-04-30 14:51:13 +0200

    Update ModalDialogProps to make transitionTimeout prop optional

--- src/interfaces/components/ModalDialogProps.ts
+++ src/interfaces/components/ModalDialogProps.ts
@@ -3,5 +3,5 @@ import { ModalDialogProps as ModalDialogPropsJoy } from '@mui/joy/ModalDialog';
 export default interface ModalDialogProps extends ModalDialogPropsJoy {
     open: boolean
     setOpen: (open: boolean) => void
-    transitionTimeout: number
+    transitionTimeout?: number
 }
