16dbbd5a5ac8176ba9f8efcf5d583415b1df6b8e
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-04-30 13:29:18 +0200

    Add shade-generator package and update Theme.tsx to use it for generating color shades

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export * from './components';
 export * from './interfaces/components';
-export { default as CssVarsProvider, extendTheme, useTheme, useThemeMaterial } from './Theme';
+export { default as CssVarsProvider, extendTheme, extendThemeMaterial, get10ColorShades, useTheme, useThemeMaterial } from './Theme';
 export * from './types';
 
