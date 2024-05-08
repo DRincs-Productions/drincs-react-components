8aff63319431cb50103b3edc85005ad6aa3edcaf
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-05-08 11:09:57 +0200

    feat: Add interfaces export in index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export { Box, Grid, Typography } from '@mui/joy';
 export { Fab } from '@mui/material';
 export * from './components';
 export * from './functions';
+export * from './interfaces';
 export * from './interfaces/components';
 export { default as CssVarsProvider, createPaletteRange, extendTheme, extendThemeMaterial, get10ColorShades, useTheme, useThemeMaterial } from './Theme';
 export * from './types';
