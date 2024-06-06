f5715580998be1aa9e7a6b1b1199caab4b6c9579
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-06-06 10:08:01 +0200

    refactor: Update ModalDialog component to use AnimatePresence for smoother transitions

--- src/components/ModalDialog.tsx
+++ src/components/ModalDialog.tsx
@@ -1,6 +1,6 @@
 import Modal from '@mui/joy/Modal';
 import { default as ModalDialogJoy } from '@mui/joy/ModalDialog';
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { useEffect, useState } from 'react';
 import ModalDialogProps from '../interfaces/components/ModalDialogProps';
 
@@ -20,43 +20,55 @@ export default function ModalDialog({ sx, open, setOpen, transitionTimeout = 400
     }, [open])
 
     return (
-        <Modal
-            keepMounted
-            open={internalOpen}
-            onClose={() => setOpen(false)}
-            component={motion.div}
-            initial={{
-                opacity: 0
-            }}
-            animate={{
-                opacity: open ? 1 : 0,
-            }}
-            exit={{
-                opacity: 0
-            }}
-            transition={{
-                duration: transitionTimeout / 1000,
-            }}
-        >
-            <ModalDialogJoy
-                sx={{
-                    ...sx,
-                }}
+        <AnimatePresence>
+            <Modal
+                keepMounted
+                open={internalOpen}
+                onClose={() => setOpen(false)}
                 component={motion.div}
-                initial={{
-                    opacity: 0,
+                variants={{
+                    open: {
+                        opacity: 1,
+                        pointerEvents: "auto",
+                        backdropFilter: "blur(8px)",
+                    },
+                    closed: {
+                        opacity: 0,
+                        pointerEvents: "none",
+                        backdropFilter: "blur(0px)",
+                    }
                 }}
-                animate={{
-                    opacity: open ? 1 : 0,
-                }}
-                exit={{
-                    opacity: 0,
-                }}
+                initial={"closed"}
+                animate={open ? "open" : "closed"}
+                exit={"closed"}
                 transition={{
-                    duration: transitionTimeout / 1000 * 0.75,
+                    duration: transitionTimeout / 1000,
                 }}
-                {...rest}
-            />
-        </Modal>
+            >
+                <ModalDialogJoy
+                    sx={{
+                        ...sx,
+                    }}
+                    component={motion.div}
+                    variants={{
+                        open: {
+                            opacity: 1,
+                            pointerEvents: "auto",
+                        },
+                        closed: {
+                            opacity: 0,
+                            pointerEvents: "none",
+                        }
+                    }}
+                    initial={"closed"}
+                    animate={internalOpen ? "open" : "closed"}
+                    exit={"closed"}
+                    transition={{
+                        duration: transitionTimeout / 1000 * 0.75,
+                    }}
+                    {...rest}
+                />
+            </Modal>
+        </AnimatePresence>
     );
 }
